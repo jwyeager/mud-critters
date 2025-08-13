a13862a42584fbf78b0046908c0d4b3e6626a9ef
jwyeager <Joel.Yeager@usm.edu>
2025-08-13 10:56:39 -0500

    xxxxxxx

--- Bentho_Data_Prep.R
+++ Bentho_Data_Prep.R
@@ -12,4 +12,12 @@ d.phylum <- read.csv("Updated_Taxonomy_PhylumDown.csv")
 
 d.all <- read.csv("Benthic infauna Summer 2022 RAW.csv")
 
-d.station <- read.csv("EMEL_StationName_log_20221207_MJA.csv")
+d.station <- read.csv("RecHist.csv")
+
+######
+## unify station nomenclature
+d.station$sta <- str_replace(d.station$Station, "BI_", "") # remove dumbass prefix
+(sta.list <- unique(d.all$Station)) # list of stations where BI sampled
+
+d.station.sub <- d.station %>% subset(sta %in% sta.list) #subset for stations sampled
+(ballsack <- unique(d.station.sub$sta)) # for comparison
