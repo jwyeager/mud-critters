50a44c66bcbc1d23a014924164b586a12870ff8e
jwyeager <Joel.Yeager@usm.edu>
2025-08-13 10:17:26 -0500

    add station name data with coords, not comprehensive

--- Bentho_Data_Prep.R
+++ Bentho_Data_Prep.R
@@ -11,3 +11,5 @@ d.order <- read.csv("Updated_Taxonomy_OrderDown.csv")
 d.phylum <- read.csv("Updated_Taxonomy_PhylumDown.csv")
 
 d.all <- read.csv("Benthic infauna Summer 2022 RAW.csv")
+
+d.station <- read.csv("EMEL_StationName_log_20221207_MJA.csv")
