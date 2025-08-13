3efed13c879d91b4a3c09132882ca90ad9a3d597
jwyeager <Joel.Yeager@usm.edu>
2025-08-13 09:44:42 -0500

    upload initial files

---
+++ Bentho_Data_Prep.R
@@ -0,0 +1,12 @@
+#########################################
+#                                       #
+#      Bentho Data Un-fucking 2025      #
+#                                       #
+#########################################
+library(tidyverse)
+library(magrittr)
+library(stringr)
+
+d.order <- read.csv("Updated_Taxonomy_OrderDown.csv")
+d.phylum <- read.csv("Updated_Taxonomy_PhylumDown.csv")
+
